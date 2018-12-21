397da61bff4ad2a341d00a7f7e177182216a4c40
Rachel Wong <rwrachelwong@gmail.com>
2018-12-21 04:28:10 +0000

    merge changes

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import App from './_app.js';
-import NavBar from './_navbar.js';
-
-const Main = () => (
-  <Router>
-    <Switch>
-    <div>
-      <NavBar />
-      <App />
-    </div>
-    </Switch>
-  </Router>
-);
-
-
-=======
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './_navbar.js';
@@ -68,5 +49,4 @@ class Main extends Component {
   }
 }
 
->>>>>>> master
 export default Main;
--- app/javascript/components/_app.js.jsx
+++ app/javascript/components/_app.js.jsx
@@ -3,12 +3,9 @@ import { Link, Route } from 'react-router-dom';
 import Modal from 'react-responsive-modal';
 
 import Quotes from "./_poli-quotes.js.jsx";
-<<<<<<< HEAD
 import Topic from "./_topic.js";
 import TabTopic from './_tabtopic.js.jsx';
-=======
 import axios from 'axios';
->>>>>>> master
 
 class App extends React.Component {
   state = {
@@ -62,56 +59,7 @@ componentDidMount() {
         <Route exact={true} path="/" render={() => (
           <div>
             <h2>Current Topics</h2>
-<<<<<<< HEAD
-            <div className="topics">
-              <div className="card-deck section group">
-                <div className="card col span_1_of_3 hometopics">
-                  <Link to="/topics/blah" className="title">
-                      <span className="card-body topic">KeyStone XL</span>
-                      <img src={require('../../assets/images/keystone.jpg')} alt="keystone-pipeline" className="img-responsive" />
-                  </Link>
-                </div>
-                <div className="card col span_1_of_3 hometopics">
-                  <Link to="/topics/cannabis" className="title">
-                    <span className="topic">Legalised Cannabis</span>
-                    <img src={require('../../assets/images/cannabis.jpg')} alt="cannabis" className="img-responsive" />
-                  </Link>
-                </div>
-                <div className="card col span_1_of_3 hometopics">
-                  <Link to="/topics/keystone" className="title">
-                      <span className="card-body topic">KeyStone XL</span>
-                      <img src='https://i.ibb.co/74f0L8w/cannabis.jpg' alt="keystone-pipeline" className="img-responsive" />
-                  </Link>
-                </div>
-              </div>
-              <div className="card-deck section group">
-                <div className="card col span_1_of_3 hometopics">
-                  <Link to="/topics/representation" className="title">
-                    <span className="topic">Proportional Representation</span>
-                    <img src={require('../../assets/images/prop-rep.jpg')} alt="proportional-representation" className="img-responsive" />
-                  </Link>
-                </div>
-                <div className="card col span_1_of_3 hometopics">
-                  <Link to="/topics/representation" className="title">
-                    <span className="topic">Subsidized Housing</span>
-                    <img src={require('../../assets/images/prop-rep.jpg')} alt="proportional-representation" className="img-responsive" />
-                  </Link>
-                </div>
-                <div className="card col span_1_of_3 hometopics">
-                  <Link to="/topics/representation" className="title">
-                    <span className="topic">Prince Eric's Hair</span>
-                    <img src={require('../../assets/images/prop-rep.jpg')} alt="proportional-representation" className="img-responsive" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}/>
-        <Route path="/topics/blah" component={Topic} />
-        <Route path="/topics/cannabis" component={TabTopic} />
-=======
-            {
-              this.state.topics.map((topic, index) => {
+            { this.state.topics.map((topic, index) => {
                 return(
                   <div className="col span_1_of_3 hometopics" key={topic.id}>
                     <Link to={`/topics/${topic.name}`} className="title">
@@ -126,7 +74,6 @@ componentDidMount() {
        <Route path="/topics" component={(Topic) => (
          this.props.match
        )}/>
->>>>>>> master
       </div>
     )
   };
