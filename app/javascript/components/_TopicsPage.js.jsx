6170e74dde330265f9ee4bad9c3f09594cdf2b1d
Rachel Wong <rwrachelwong@gmail.com>
2018-12-21 05:27:44 +0000

    change classNames on topics

--- app/javascript/components/_TopicsPage.js.jsx
+++ app/javascript/components/_TopicsPage.js.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 
 
 function TopicsPage({ topics }) {
+  console.log('what is the arrayy?@@?@?@', topics);
   return (
     <div>
       <h2>Current Topics</h2>
       <div className="topics">
         <div className="card-deck section group">
-          { topics.map((topic) => { 
+          { topics.map((topic) => {
             return (
-              <div className="card col span_1_of_3 hometopics" key={topic.id}>
+              <div className="col-4 span_1_of_3 hometopics" key={topic.id}>
                 <Link to={`/topics/${topic.name}/${topic.id}`} className="title" params={{ id: topic.id }}>
                   <span className="topic">{topic.name}</span>
                   <img src={topic.photo_url} alt={topic.name} className="img-responsive" />
