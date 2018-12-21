cc119de89ab187d8bbf31a6206acf4a3a1eacd02
Rachel Wong <rwrachelwong@gmail.com>
2018-12-21 05:57:23 +0000

    add modal to NavBar component and add padding to topics

--- app/javascript/components/_navbar.js.jsx
+++ app/javascript/components/_navbar.js.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-responsive-modal';
+import Quotes from "./_poli-quotes.js.jsx";
 
-const NavBar = () => (
-  <div className="navbar">
-    <Link to="/" className="name">KNOW YOUR VOTE</Link>
-  </div>
-);
+
+class NavBar extends React.Component {
+  state = {
+    open: false,
+    topics: [],
+    currentTopic: null
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div>
+        <div className="navbar">
+          <Link to="/" className="name">KNOW YOUR VOTE</Link>
+          <span className="poli-match-button">
+          <button type="button" onClick={this.onOpenModal}>Find my Poli-match</button>
+        </span>
+        </div>
+        <div>
+          <Modal open={open} onClose={this.onCloseModal} center>
+            <h2>Find Your Match</h2>
+            <Quotes />
+            <button>Disagree</button>
+            <button>Agree</button>
+          </Modal>
+        </div>
+      </div>
+    )
+  };
+}
 
 export default NavBar;
