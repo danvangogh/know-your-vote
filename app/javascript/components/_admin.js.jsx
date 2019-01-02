d2bc649ac6a8325b8beeee50382faa7c5308a8bd
Rachel Wong <rwrachelwong@gmail.com>
2019-01-02 19:24:18 +0000

    add bootstrap styling to topic form

--- app/javascript/components/_admin.js.jsx
+++ app/javascript/components/_admin.js.jsx
@@ -12,11 +12,28 @@ class Admin extends Component {
   render() {
     return (
       <form>
-        <label>
-          Topic:
-          <input type='text' name='name' />
-        </label>
-        <input type="submit" value="Submit"/>
+        <h4 class="add-topic">Add a Topic</h4>
+        <div class="form-group">
+          <label for="topic-name">Name</label>
+          <input type="text" class="form-control"></input>
+        </div>
+        <div class="form-group">
+          <label for="topic-description">Description</label>
+          <textarea class="form-control" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="topic-description">Good</label>
+          <textarea class="form-control" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="topic-description">Bad</label>
+          <textarea class="form-control" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="topic-name">Twitter collection URL</label>
+          <input type="text" class="form-control"></input>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </form>
     )
   }
