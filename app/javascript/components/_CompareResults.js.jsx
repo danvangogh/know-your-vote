eaeb67b8381237fa963f519f39471af98f4547ed
danvangogh <djameswhite@yahoo.com>
2019-01-04 19:22:40 +0000

    add compareResults component

---
+++ app/javascript/components/_CompareResults.js.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+class CompareResults extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      grnRes: [3, 6, 6, 8, 4, 0],
+      ndpRes: [0, 0, 1, 6, 3, 2],
+      libRes: [0, 2, 2, 2, 5, 2],
+      cpRes: [1, 5, 8, 8, 4, 1],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/results')
+    .then((response) => {
+      this.setState({
+        grnRes: [],
+        ndpRes: [],
+        libRes: [],
+        cpRes: [],
+      })
+    })
+    .catch(function (error) {
+      return error;
+    });
+  }
+
+  const calc = res => {
+    for (let party in res) {
+      res[party] = res[party].reduce((a,b) => a + b, 0) / res[party].length
+    }
+    return res
+  }
+
+
+
+  render() {
+    return(
+
+    )
+  }
+}
