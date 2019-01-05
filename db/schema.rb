9d78c22dbe634082eb50d6e78d6cfb20a85c45c5
danvangogh <djameswhite@yahoo.com>
2019-01-05 23:05:07 +0000

    adds vote-results table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190103010156) do
+ActiveRecord::Schema.define(version: 20190105225112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20190103010156) do
     t.datetime "updated_at", null: false
     t.string "photo_url"
     t.string "twitter_url"
+  end
+
+  create_table "vote_results", force: :cascade do |t|
+    t.integer "grn"
+    t.integer "ndp"
+    t.integer "lib"
+    t.integer "cp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "quotes", "parties"
